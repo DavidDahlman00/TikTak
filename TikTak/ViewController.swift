ce5491c1597d923e2abcfb1d5a92201a49bbb929
David Dahlman <daviddahlman82@gmail.com>
2020-12-26 21:25:55 +0100

    Start menu page

--- TikTak/ViewController.swift
+++ TikTak/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+  
 
+    @IBOutlet weak var pickerData: UIPickerView!
+    
+    private var gameDifficulty: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        gameDifficulty =  ["Hard","Normal","Easy"]
     }
-
+    
+    @IBAction func startButton(_ sender: UIButton) {
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return gameDifficulty.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       
+        return gameDifficulty[row]
+    }
 
 }
 
