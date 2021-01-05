6f152f1eba585b3890620aaa0b33152374e18eca
David Dahlman <daviddahlman82@gmail.com>
2021-01-05 10:45:41 +0100

    Not Working! UICollectionView

---
+++ TikTak/GameViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  GameViewCell.swift
+//  TikTak
+//
+//  Created by David Dahlman on 2021-01-05.
+//
+
+import UIKit
+
+class GameViewCell: UICollectionViewCell {
+    static let identifier = "GameViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        
+        imageView.image = UIImage(named: "ticTacImage")
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+}
